2fbf687c20f396539ad56a506e2805d79417f7da
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-12-22 16:12:48 -0700

    modified resolver and schema to have company in job

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,17 +1,13 @@
+import { getCompany } from './db/companies.js'
+import { getJobs } from './db/jobs.js'
+
 export const resolvers = {
     Query: {
-        jobs: () => {
-            return [{
-                id: '1',
-                title: 'The Title',
-                description: 'The description '
-            },
-            {
-                id: '2',
-                title: 'The Title 2 ',
-                description: 'The description '
-            },
-        ]
-        }
+        jobs: () => getJobs()
+    },
+
+    Job: {
+        date: (job) => job.createdAt.slice(0, 'yyyy-mm-dd'.length),
+        company: async (job) => getCompany(job.companyId)
     }
 }
