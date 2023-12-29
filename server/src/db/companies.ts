dce97299cbbd093d596ef21a8815cb8416aaac91
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-12-28 22:40:49 -0700

    initialized typescript with graphql

--- server/src/db/companies.ts
+++ server/src/db/companies.ts
@@ -1,15 +1,15 @@
-import { connection } from './connection.js';
 import DataLoader from 'dataloader';
+import { connection } from './connection.js';
+import { CompanyEntity } from './types.js';
 
-const getCompanyTable = () => connection.table('company');
+const getCompanyTable = () => connection.table<CompanyEntity>('company');
 
-export async function getCompany(id) {
+export async function getCompany(id: string) {
   return await getCompanyTable().first().where({ id });
 }
 
 export function createCompanyLoader() {
-  return new DataLoader(async (ids) => {
-    console.log('[companyLoader] ids:', ids);
+  return new DataLoader(async (ids: string[]) => {
     const companies = await getCompanyTable().select().whereIn('id', ids);
     return ids.map((id) => companies.find((company) => company.id === id));
   });
